4e6728ea389caed0e214bf765354159e09a171a8
DaniyalAli7778 <daniyal.personel7778@gmail.com>
2024-11-25 12:59:56 +0500

    "Initialize Repozitory "

---
+++ pintrest/pintrest.js
@@ -0,0 +1,96 @@
+var arr = [
+  { name: "Petals of roses", image: "https://images.unsplash.com/photo-1518621736915-f3b1c41bfd00?q=80&w=3786&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+  { name: "Animals of town", image: "https://images.unsplash.com/photo-1583337130417-3346a1be7dee?q=80&w=2688&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", },
+  { name: "the crowd of city", image: "https://images.unsplash.com/photo-1517732306149-e8f829eb588a?q=80&w=3872&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", },
+  { name: "fruits of planet", image: "https://images.unsplash.com/photo-1618897996318-5a901fa6ca71?q=80&w=3764&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", },
+  { name: "orange peeled", image: "https://images.unsplash.com/photo-1557800636-894a64c1696f?q=80&w=3337&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", },
+  { name: "web design", image: "https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", },
+  { name: "apple juice", image: "https://images.unsplash.com/photo-1576673442511-7e39b6545c87?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", }
+]
+
+function showtheCart() {
+  var culter = "";
+  arr.forEach(function (obj) {
+
+    culter += `<div class="  " id="box"  >
+
+    <img class="cursor-pointer" src="${obj.image}" alt="${obj.name} "  >
+    <div class="caption">   </div>
+</div>`
+
+      ;
+
+  })
+
+
+
+  document.querySelector('.container')
+
+
+    .innerHTML = culter;
+}
+
+
+function inputhandleFunctionality() {
+
+  let changeOverLay = document.querySelector('#searchinput');
+
+
+  changeOverLay.addEventListener('focus', function () {
+
+
+    document.querySelector('.overlay')
+
+
+      .style.display = "block";
+
+  })
+
+
+
+  changeOverLay.addEventListener('blur', function () {
+
+
+    document.querySelector('.overlay')
+
+
+      .style.display = "none";
+
+  })
+  changeOverLay.addEventListener('input', () => {
+
+    var filteredArray = arr.filter((obj) => {
+
+
+      return obj.name.startsWith(changeOverLay.value)
+
+    }
+
+
+
+    );
+    var cullter = "";
+    filteredArray.forEach(function (obj) {
+      cullter += `<div class="res flex px-8 py-3">
+            <i class="ri-search-line font-semibold mr-5"></i>
+            <h3 class=" suggestions font-semibold">${obj.name}</h3>
+        </div>` ;
+
+
+    })
+
+    document.querySelector('.searchdata').style.display = "block";
+    document.querySelector('.searchdata').innerHTML = cullter;
+
+
+
+
+
+    console.log(filteredArray)
+
+  })
+
+
+}
+inputhandleFunctionality();
+showtheCart();
