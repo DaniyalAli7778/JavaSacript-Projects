f7db1263a3d455ed5da0bcccf6cfa5aed8e2ea96
DaniyalAli7778 <daniyal.personel7778@gmail.com>
2024-12-01 09:06:38 +0500

    "commit"

--- Music-Player/script.js
+++ Music-Player/script.js
@@ -1,13 +1,13 @@
-let Songs =[{name:"Pehla ma bhi", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyMgI6PR7z6WXtVeeRSpHbY1r05Dt7BeBWXgueZGQohjMcH2ykR5rXvhTorgPe29Df6wM&usqp=CAU", duration:"3:56"},
+let songs =[{name:"Pehla ma bhi", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyMgI6PR7z6WXtVeeRSpHbY1r05Dt7BeBWXgueZGQohjMcH2ykR5rXvhTorgPe29Df6wM&usqp=CAU", duration:"3:56"},
 
-  {name:"Pehla ma bhi", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyMgI6PR7z6WXtVeeRSpHbY1r05Dt7BeBWXgueZGQohjMcH2ykR5rXvhTorgPe29Df6wM&usqp=CAU", duration:"3:56" },
+  {name:"Chai", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyMgI6PR7z6WXtVeeRSpHbY1r05Dt7BeBWXgueZGQohjMcH2ykR5rXvhTorgPe29Df6wM&usqp=CAU", duration:"3:56" },
   {name:"Pehla ma bhi", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyMgI6PR7z6WXtVeeRSpHbY1r05Dt7BeBWXgueZGQohjMcH2ykR5rXvhTorgPe29Df6wM&usqp=CAU", duration:"3:56" },
   {name:"Pehla ma bhi", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyMgI6PR7z6WXtVeeRSpHbY1r05Dt7BeBWXgueZGQohjMcH2ykR5rXvhTorgPe29Df6wM&usqp=CAU", duration:"3:56" }
 ];
 
 function showSongs(){
 let cullter ="";
-Songs.forEach((song)=>{
+let songsList =songs.map((song)=>{
   cullter=`<div class="song-card">
             <div class="card-part-1">
               <img
@@ -20,8 +20,12 @@ Songs.forEach((song)=>{
              
             <h6>${song.duration}</h6>
           </div>`
-})
- document.querySelector('.all-songs').innerHTML=cullter;
-}
-// 
+          
+});
+document.querySelector('.all-songs').innerHTML=cullter;
+
+console.log();
+
+};
+
 showSongs();
